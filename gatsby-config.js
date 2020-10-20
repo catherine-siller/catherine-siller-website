17c983e751a59b8e7aeebc3b3250c71ba975242e
Catherine Siller <csiller@mailgun.com>
2020-10-19 23:21:12 -0500

    Add /blog and Markdown support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,19 @@
 module.exports = {
+  siteMetadata: {
+    title: `Catherine Siller Blog`,
+    siteURL: 'https://www.catherine-siller.com/',
+    siteName: 'Catherine Siller Blog',
+    author: `Catherine Siller`
+  },
   plugins: [
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`
   ],
 }
